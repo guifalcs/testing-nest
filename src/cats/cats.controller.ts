8ecfe21b444e02d5db01577a1625fca68186ed90
Guilherme Falcão <guilherme.falcao@unvoid.com>
2025-04-15 11:48:03 -0300

    training error handling

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
 import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
 
-    constructor(private catsService: CatsService) {}
-
-    @Get()
-    findAll(): string[] {
-        return this.catsService.findAll();
+  @Get(':id')
+  findAll(@Param('id') id: string): string[] {
+    if (id) {
+      throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
     }
+    return this.catsService.findAll();
+  }
 }
